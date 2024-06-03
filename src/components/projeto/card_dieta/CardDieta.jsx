4b93b0fe7e04287a64ba721bbede90c223d0c3bb
Cabayer915 <caua.bonfim@sptech.school>
2024-06-03 15:31:25 -0300

    Alterações na Dashboard da tela de dieta, JSON iterado com a req do GPT

--- src/components/projeto/card_dieta/CardDieta.jsx
+++ src/components/projeto/card_dieta/CardDieta.jsx
@@ -1,144 +1,207 @@
 import React, { useState, useEffect } from "react";
-import styles from './CardDieta.module.css';
+import styles from "./CardDieta.module.css";
 import img1 from "../../../utils/cafe-da-manha.jpg";
 import img2 from "../../../utils/almoco.jpg";
 import img3 from "../../../utils/lanche.jpg";
 import img4 from "../../../utils/jantar.jpg";
 import img5 from "../../../utils/ceia.webp";
 import ModalDieta from "../modal_dieta/ModalDieta";
-import Api from "../../../api"
-import { getId } from "../../../service/auth"
+import api from "../../../api";
+import CircularProgress from "@mui/material/CircularProgress";
+import { getId } from "../../../service/auth";
 
-const CardDieta = () => {
-    const [cardsData, setCardsData] = useState([]);
-    const [selectedCard, setSelectedCard] = useState(null);
-    const [showSelectModal, setShowSelectModal] = useState(true);
-    const [qtdCards, setQtdCards] = useState(3);
+const CardDieta = ({ onNutrientTotalsUpdate, onCurrentNutrientUpdate }) => {
+  const [loading, setLoading] = useState(false);
+  const [cardsData, setCardsData] = useState([]);
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [showSelectModal, setShowSelectModal] = useState(true);
+  const [qtdCards, setQtdCards] = useState(3);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
+  const predefinedCards = [
+    { id: 1, horario: "Café da Manhã - 9:00 AM", img: img1 },
+    { id: 2, horario: "Almoço - 12:00 PM", img: img2 },
+    { id: 3, horario: "Lanche da tarde - 15:00 PM", img: img3 },
+    { id: 4, horario: "Jantar - 19:00 PM", img: img4 },
+    { id: 5, horario: "Ceia - 22:00 PM", img: img5 },
+  ];
 
-        // try {
-        //     const response = await Api.get(`pesos/historico-grafico/${getId()}`);
-        //     console.log(response)
-        // } catch (error) {
-        //     console.error(error);
-        // }
-
-        if (!showSelectModal) {
-            const fetchData = () => {
-                const data = [];
-                for (let i = 0; i < qtdCards; i++) {
-                    const id = i + 1;
-                    let horario = '';
-                    let img = '';
-
-                    switch (id) {
-                        case 1:
-                            horario = "Café da Manhã - 9:00 AM";
-                            img = img1;
-                            break;
-                        case 2:
-                            horario = "Almoço - 12:00 PM";
-                            img = img2;
-                            break;
-                        case 3:
-                            horario = "Lanche da tarde - 15:00 PM";
-                            img = img3;
-                            break;
-                        case 4:
-                            horario = "Jantar - 19:00 PM";
-                            img = img4;
-                            break;
-                        case 5:
-                            horario = "Ceia - 22:00 PM";
-                            img = img5;
-                            break;
-                        default:
-                            break;
-                    }
-
-                    data.push({
-                        id,
-                        titulo: `Título ${id}`,
-                        descricao: `Descrição ${id}`,
-                        horario,
-                        img
-                    });
-                }
-                setCardsData(data);
-            };
-
-            fetchData();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const id = getId();
+        if (!id) {
+          throw new Error("ID não encontrado");
         }
-    }, [qtdCards, showSelectModal]);
 
-    const handleGreenButtonClick = (card) => {
-        setSelectedCard(card);
+        const response = await api.get(`/openai/gpt3/${id}`, {
+          params: { objetivo: "Perder peso" },
+        });
+
+        const apiData = response.data;
+        console.log(apiData);
+
+        const updatedCardsData = predefinedCards
+          .slice(0, qtdCards)
+          .map((card, index) => ({
+            ...card,
+            titulo: apiData[index]?.nome || `Título ${index + 1}`,
+            descricao: apiData[index]?.modoPreparo || `Descrição ${index + 1}`,
+            carboidratos: Number(apiData[index]?.carboidratos) || 0,
+            calorias: Number(apiData[index]?.calorias) || 0,
+            gorduras: Number(apiData[index]?.gorduras) || 0,
+            proteinas: Number(apiData[index]?.proteina) || 0,
+            acucares: apiData[index]?.acucar,
+            tempoPreparo: apiData[index]?.tempoPreparo,
+            ingredientes: apiData[index]?.ingredientes,
+          }));
+
+        setCardsData(updatedCardsData);
+        sessionStorage.setItem("cardsData", JSON.stringify(updatedCardsData));
+
+        const totalNutrients = updatedCardsData.reduce(
+          (totals, card) => {
+            return {
+              carboidratos: totals.carboidratos + card.carboidratos,
+              calorias: totals.calorias + card.calorias,
+              gorduras: totals.gorduras + card.gorduras,
+              proteinas: totals.proteinas + card.proteinas,
+            };
+          },
+          { carboidratos: 0, calorias: 0, gorduras: 0, proteinas: 0 }
+        );
+
+        sessionStorage.setItem("totalNutrients", JSON.stringify(totalNutrients));
+        onNutrientTotalsUpdate(totalNutrients);
+      } catch (error) {
+        console.error("Erro na requisição:", error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    const handleRedButtonClick = (id) => {
-        const updatedCards = cardsData.filter(card => card.id !== id);
-        setCardsData(updatedCards);
-    };
+    const storedCardsData = sessionStorage.getItem("cardsData");
+    const storedTotalNutrients = sessionStorage.getItem("totalNutrients");
 
-    const closeModal = () => {
-        setSelectedCard(null);
-    };
+    if (storedCardsData && storedTotalNutrients) {
+      const parsedCardsData = JSON.parse(storedCardsData);
+      const parsedTotalNutrients = JSON.parse(storedTotalNutrients);
+      setCardsData(parsedCardsData);
+      setShowSelectModal(false);
+      onNutrientTotalsUpdate(parsedTotalNutrients);
+    } else {
+      if (!showSelectModal) {
+        fetchData();
+      }
+    }
+  }, [qtdCards, showSelectModal, onNutrientTotalsUpdate]);
 
-    const handleSelectChange = (e) => {
-        setQtdCards(Number(e.target.value));
-    };
+  const handleGreenButtonClick = (card) => {
+    setSelectedCard(card);
+  };
 
-    const handleStartClick = () => {
-        setShowSelectModal(false);
-    };
+  const handleRedButtonClick = (id) => {
+    const updatedCards = cardsData.filter((card) => card.id !== id);
+    const completedCard = cardsData.find((card) => card.id === id);
 
-    return (
-        <div className={styles.cards_listener}>
-            {showSelectModal && (
-                <div className={styles.modal}>
-                    <div className={styles.modal_content}>
-                        <h2>Selecione o número de refeições que deseja fazer hoje</h2>
-                        <select value={qtdCards} onChange={handleSelectChange}>
-                            <option value={3}>3</option>
-                            <option value={4}>4</option>
-                            <option value={5}>5</option>
-                        </select>
-                        <button onClick={handleStartClick}>Iniciar</button>
-                    </div>
-                </div>
-            )}
+    if (completedCard) {
+      onCurrentNutrientUpdate((prevTotals) => {
+        const newTotals = {
+          carboidratos: prevTotals.carboidratos + completedCard.carboidratos,
+          calorias: prevTotals.calorias + completedCard.calorias,
+          gorduras: prevTotals.gorduras + completedCard.gorduras,
+          proteinas: prevTotals.proteinas + completedCard.proteinas,
+        };
+        sessionStorage.setItem("currentNutrientTotals", JSON.stringify(newTotals));
+        return newTotals;
+      });
+    }
 
-            {cardsData.length === 0 && !showSelectModal && (
-                <div className={styles.parabens}>Parabéns por ter completado suas refeições! Volte amanhã 😊</div>
-            )}
-            {cardsData.map((card) => (
-                <div key={card.id} className={styles.card_container}>
-                    <div className={styles.horario}>{card.horario}</div>
-                    <div id={`card-${card.id}`} className={styles.card_box}>
-                        <div className={styles.title_box}>
-                            <div className={styles.title}>{card.titulo}</div>
-                        </div>
-                        <div className={styles.img_box}>
-                            <img className={styles.img} alt="" src={card.img} />
-                        </div>
-                        <div className={styles.box_desc}>
-                            <div className={styles.sub_text}>Sobre a Refeição:</div>
-                            <div className={styles.sub_text}>{card.descricao}</div>
-                        </div>
-                        <button onClick={() => handleGreenButtonClick(card)} className={styles.button_green}>
-                            <div className={styles.text_button}>Detalhes</div>
-                        </button>
-                        <button onClick={() => handleRedButtonClick(card.id)} className={styles.button_red}>
-                            <div className={styles.text_button}>Concluir Refeição</div>
-                        </button>
-                    </div>
-                </div>
-            ))}
+    setCardsData(updatedCards);
+    sessionStorage.setItem("cardsData", JSON.stringify(updatedCards));
+  };
 
-            {selectedCard && <ModalDieta card={selectedCard} onClose={closeModal} />}
+  const closeModal = () => {
+    setSelectedCard(null);
+  };
+
+  const handleSelectChange = (e) => {
+    setQtdCards(Number(e.target.value));
+  };
+
+  const handleStartClick = () => {
+    setShowSelectModal(false);
+  };
+
+  return (
+    <div className={styles.cards_listener}>
+      {showSelectModal && !sessionStorage.getItem("cardsData") && (
+        <div className={styles.modal}>
+          <div className={styles.modal_content}>
+            <h2>Selecione o número de refeições que deseja fazer hoje</h2>
+            <select value={qtdCards} onChange={handleSelectChange}>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
+            </select>
+            <button onClick={handleStartClick}>Iniciar</button>
+          </div>
         </div>
-    );
+      )}
+
+      {loading && (
+        <div className={styles.modal}>
+          <div className={styles.modal_content}>
+            <h2>Carregando suas refeições, aguarde...</h2>
+            <div className={styles.loading}>
+              <CircularProgress color="primary" />
+            </div>
+          </div>
+        </div>
+      )}
+
+      {error && <p>Erro: {error}</p>}
+
+      {cardsData.length === 0 && !showSelectModal && (
+        <div className={styles.parabens}>
+          Parabéns por ter completado suas refeições! Volte amanhã 😊
+        </div>
+      )}
+      {cardsData.map((card) => (
+        <div key={card.id} className={styles.card_container}>
+          <div className={styles.horario}>{card.horario}</div>
+          <div id={`card-${card.id}`} className={styles.card_box}>
+            <div className={styles.title_box}>
+              <div className={styles.title}>{card.titulo}</div>
+            </div>
+            <div className={styles.img_box}>
+              <img className={styles.img} alt="" src={card.img} />
+            </div>
+            <div className={styles.box_desc}>
+              <div className={styles.sub_text}>Sobre a Refeição:</div>
+              <div className={styles.sub_text}>{card.descricao}</div>
+            </div>
+            <button
+              onClick={() => handleGreenButtonClick(card)}
+              className={styles.button_green}
+            >
+              <div className={styles.text_button}>Detalhes</div>
+            </button>
+            <button
+              onClick={() => handleRedButtonClick(card.id)}
+              className={styles.button_red}
+            >
+              <div className={styles.text_button}>Concluir Refeição</div>
+            </button>
+          </div>
+        </div>
+      ))}
+
+      {selectedCard && <ModalDieta card={selectedCard} onClose={closeModal} />}
+    </div>
+  );
 };
 
 export default CardDieta;
--- src/components/projeto/modal_dieta/ModalDieta.jsx
+++ src/components/projeto/modal_dieta/ModalDieta.jsx
@@ -4,6 +4,15 @@ import styles from './ModalDieta.module.css';
 const ModalDieta = ({ card, onClose }) => {
     if (!card) return null;
 
+    const calcularPorcentagem = (valorAtual, valorMaximo) => {
+        return (valorAtual / valorMaximo) * 100;
+    };
+
+    const determinarClasse = (valorAtual, valorMaximo) => {
+        const porcentagem = calcularPorcentagem(valorAtual, valorMaximo);
+        return porcentagem >= 80 ? styles.scaleActive : styles.scaleNone;
+    };
+
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.modal}>
@@ -15,70 +24,74 @@ const ModalDieta = ({ card, onClose }) => {
                     <img src={card.img} alt={card.titulo} className={styles.img} />
                     <div className={styles.info}>
                         <div className={styles.sectionInfo}>
-                        <h2 className={styles.font_secondary}>Calorias</h2>
-                        <h2 className={styles.font_secondary}>32{}g</h2>
+                            <h2 className={styles.font_secondary}>Calorias</h2>
+                            <h2 className={styles.font_secondary}>{card.calorias}kcal</h2>
                         </div>
                         <div className={styles.scale}>
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
+                            <div className={determinarClasse(card.calorias, 50)} />
+                            <div className={determinarClasse(card.calorias, 100)} />
+                            <div className={determinarClasse(card.calorias, 200)} />
+                            <div className={determinarClasse(card.calorias, 300)} />
+                            <div className={determinarClasse(card.calorias, 400)} />
+                            <div className={determinarClasse(card.calorias, 500)} />
                         </div>
                         <div className={styles.sectionInfo}>
-                        <h2 className={styles.font_secondary}>Proteínas</h2>
-                        <h2 className={styles.font_secondary}>32{}g</h2>
+                            <h2 className={styles.font_secondary}>Proteínas</h2>
+                            <h2 className={styles.font_secondary}>{card.proteinas}g</h2>
                         </div>
                         <div className={styles.scale}>
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
-                            </div>
+                            <div className={determinarClasse(card.proteinas, 10)} />
+                            <div className={determinarClasse(card.proteinas, 20)} />
+                            <div className={determinarClasse(card.proteinas, 30)} />
+                            <div className={determinarClasse(card.proteinas, 40)} />
+                            <div className={determinarClasse(card.proteinas, 50)} />
+                            <div className={determinarClasse(card.proteinas, 60)} />
+                        </div>
                         <div className={styles.sectionInfo}>
-                        <h2 className={styles.font_secondary}>Carboitrados</h2>
-                        <h2 className={styles.font_secondary}>32{}g</h2>
+                            <h2 className={styles.font_secondary}>Carboidratos</h2>
+                            <h2 className={styles.font_secondary}>{card.carboidratos}g</h2>
                         </div>
                         <div className={styles.scale}>
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleNone} />
-                            </div>
+                            <div className={determinarClasse(card.carboidratos, 10)} />
+                            <div className={determinarClasse(card.carboidratos, 20)} />
+                            <div className={determinarClasse(card.carboidratos, 30)} />
+                            <div className={determinarClasse(card.carboidratos, 40)} />
+                            <div className={determinarClasse(card.carboidratos, 50)} />
+                            <div className={determinarClasse(card.carboidratos, 60)} />
+                        </div>
                         <div className={styles.sectionInfo}>
-                        <h2 className={styles.font_secondary}>Gorduras</h2>
-                        <h2 className={styles.font_secondary}>32{}g</h2>
+                            <h2 className={styles.font_secondary}>Gorduras</h2>
+                            <h2 className={styles.font_secondary}>{card.gorduras}g</h2>
                         </div>
                         <div className={styles.scale}>
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
-                            </div>
+                            <div className={determinarClasse(card.gorduras, 10)} />
+                            <div className={determinarClasse(card.gorduras, 20)} />
+                            <div className={determinarClasse(card.gorduras, 30)} />
+                            <div className={determinarClasse(card.gorduras, 40)} />
+                            <div className={determinarClasse(card.gorduras, 50)} />
+                            <div className={determinarClasse(card.gorduras, 60)} />
+                        </div>
                         <div className={styles.sectionInfo}>
-                        <h2 className={styles.font_secondary}>Açucares</h2>
-                        <h2 className={styles.font_secondary}>32{}g</h2>
+                            <h2 className={styles.font_secondary}>Açúcares</h2>
+                            <h2 className={styles.font_secondary}>{card.acucares}g</h2>
                         </div>
                         <div className={styles.scale}>
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleActive} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
-                                <div className={styles.scaleNone} />
-                            </div>
+                            <div className={determinarClasse(card.acucares, 5)} />
+                            <div className={determinarClasse(card.acucares, 10)} />
+                            <div className={determinarClasse(card.acucares, 15)} />
+                            <div className={determinarClasse(card.acucares, 20)} />
+                            <div className={determinarClasse(card.acucares, 25)} />
+                            <div className={determinarClasse(card.acucares, 30)} />
+                        </div>
                     </div>
                 </div>
                 <div className={styles.modalContent}>
                     <h1 className={styles.font}>Sobre a Refeição:</h1>
-                    <p>{card.descricao}</p>
+                    <p className={styles.font_secondary}>{card.descricao}</p>
+                    <h1 className={styles.font}>Tempo de Preparo:</h1>
+                    <p className={styles.font_secondary}>{card.tempoPreparo} minutos.</p>
+                    <h1 className={styles.font}>Ingredientes:</h1>
+                    <p className={styles.font_secondary}>{card.ingredientes.join(", ")}.</p>
                 </div>
             </div>
         </div>
